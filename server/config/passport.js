821ae2787b0709a45dbf971e8d0d4c64b80e2216
andrew brown <aebrownz11@gmail.com>
2016-07-27 10:39:55 -0700

    Added local and jwt auth strategies with passport and added dependencies

---
+++ server/config/passport.js
@@ -0,0 +1,54 @@
+const passport = require('passport');
+const JwtStrategy = require('passport-jwt').Strategy();
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy;
+const config = require('./config');
+const User = require('../user/user.model');
+const compare = require('./utils').comparePassword;
+
+
+module.exports = {
+  authLocal
+};
+
+passport.use(new JwtStrategy(
+  {
+    secretOrKey: config.secret,
+    jwtFromRequest: ExtractJwt.fromAuthHeader()
+  }, (jwt_payload, done) => {
+    User.findOne({ where: { id: jwt_payload.id }})
+      .then(user => {
+        if (user) {
+          done(null, user);
+        } else {
+          done(null, false);
+        }
+      })
+      .catch(done);
+  }
+));
+
+function authLocal(req, res) {
+  User.findOne({ where: { username: req.body.username }})
+    .then(user => {
+      if (user) {
+        compare(req.body.password, user.password)
+          .then(match => {
+            if (!match) {
+              done(null, false);
+            } else {
+              req.user = user;
+              done(null);
+            }
+          });
+      }
+    });
+}
+
+
+
+
+// TODO
+// create the user model
+// add an exported secret to config
+// add a comparePassword function to utils
