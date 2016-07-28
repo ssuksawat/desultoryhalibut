5d04b6c70593f76057e98e1b4e9506be40bca51d
ssuksawat@worcester.edu <ssuksawat21@gmail.com>
2016-07-28 10:42:36 -0700

    return index.html for all other request - for client-side routing

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,8 +1,13 @@
 var TwitterRouter = require('../twitter/twitter-router');
 
 
-module.exports = function(app) {
+module.exports = function(app, config) {
 
   app.use('/api/twitter', TwitterRouter);
+
+  // Serve index.html for all other requests - for client-side routing
+  app.get('/*', (req, res) => {
+    res.sendFile(config.rootPath + '/client/www/index.html');
+  });
 
 };
