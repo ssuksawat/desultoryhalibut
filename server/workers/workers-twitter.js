a6b4e3f82bca3ec44434afe3787542ed2d38cc43
conway <conwaywang@gmail.com>
2016-07-28 11:39:41 -0700

    add dummy data to tag datbase and check stream on all tweets

--- server/workers/workers-twitter.js
+++ server/workers/workers-twitter.js
@@ -1,36 +1,60 @@
+'use strict';
+
 const twitterModels = require('../twitter/twitter-model');
 const TwitterStreamChannels = require('twitter-stream-channels');
 const config = require('../config/config')
 const sentiment = require('sentiment');
+const Topic = require('../topics/topic.model');
 
 const client = new TwitterStreamChannels(config.twitter)
 
-const channels = {
-  'nintendo' : ['nintendo'],
-  'google' : ['google'],
-  'ford': ['ford'],
-  'disney': ['disney'],
-  'genentech': ['genentech'],
-  'gold': ['gold']
-};
 
-var stream = client.streamChannels({
-  track: channels, 
-  language: "en"
-});
+let channels = {};
+let stream = {};
+
+fetchAndSubscribe();
+setInterval(fetchAndSubscribe, 30000);
 
 
-var cache = {};
+function watchChannels() {
+let cache = {};
+console.log('asdfasdf')
+  Object.keys(channels).forEach(topic => {
+    const channel = 'channels/' + topic;
+    console.log(channel);
+    const tag = '' + topic;
+    cache[tag] = {sentiment: 0, total: 0};
+    stream.on(channel, function(tweet) {
+      cache[tag].total++;
+      cache[tag].sentiment += sentiment(tweet.text).score;
+      console.log('tag', tag, 'text', tweet.text, cache[tag].total, cache[tag].sentiment);
+      });
+  });
+}
 
-Object.keys(channels).forEach(topic => {
-  var channel = 'channels/' + topic;
-  console.log(channel)
-  var tag = '' + topic;
-  cache[tag] = {sentiment: 0, total: 0};
+function fetchAndSubscribe () {
+  Topic.findAll()
+    .then(topics => { 
+      topics.forEach(function(tag) {
+        console.log('Tag: ', tag.topic);
+        const item = tag.topic;
+        channels[item] = [item];
+      });
+    })
+    .then(unsubscribe)
+    .then(subscribeToTopics)
+    .then(watchChannels);
+}
 
-  stream.on(channel, function(tweet) {
-    cache[tag].total++;
-    cache[tag].sentiment += sentiment(tweet.text).score;
-    console.log('tag', tag, 'text', tweet.text, cache[tag].total, cache[tag].sentiment)
-    });
-})
+function subscribeToTopics() {
+  stream = client.streamChannels({
+    track: channels, 
+    language: "en"
+  });
+}
+
+function unsubscribe() {
+  if (stream.off) {
+    stream.off();
+  }
+}
