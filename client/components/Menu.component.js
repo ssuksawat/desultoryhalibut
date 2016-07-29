c4e5372f04a301063c27a4bd13c3dc7125be9584
Justin Ross <Ross.Justin.T@gmail.com>
2016-07-28 19:39:55 -0700

    Implement basic structure for menu component

--- client/components/Menu.component.js
+++ client/components/Menu.component.js
@@ -1,4 +1,7 @@
 import React from 'react';
+//import AuthButton from './AuthButton.component';
+//import Subscription from './Subscription.component';
+
 
 // Initialize collapse button
 // $(".button-collapse").sideNav(); <~~~~~~~~~~~ Goes in App?
@@ -7,17 +10,10 @@ const Menu = (props) => {
 	return (
 		<div>
 			<ul id="slide-out" class="side-nav">
-			    <li><div class="userView">
-			      <img class="background" src="images/office.jpg">
-			      <a href="#!user"><img class="circle" src="images/yuna.jpg"></a>
-			      <a href="#!name"><span class="white-text name">John Doe</span></a>
-			      <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-			    </div></li>
-			    <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-			    <li><a href="#!">Second Link</a></li>
+			    <li>AuthButton</li>
 			    <li><div class="divider"></div></li>
-			    <li><a class="subheader">Subheader</a></li>
-			    <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
+			    <li><a class="subheader">Subscription stuff:</a></li>
+			    <li>Subscription</li>
 			</ul>
 			<a href="#" data-activates="slide-out" class="button-collapse"><i class="material-icons">menu</i></a>
 		</div>
